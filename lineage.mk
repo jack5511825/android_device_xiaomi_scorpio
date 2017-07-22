c24648e4e716b65090ce6da9d7413506c5eb499a
stonetrapping <tom@stonetrap.info>
2017-07-22 15:50:31 +0200

    scorpio: fingerprint change

--- lineage.mk
+++ lineage.mk
@@ -14,5 +14,5 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_DEVICE="scorpio" \
     PRODUCT_NAME="scorpio" \
-    BUILD_FINGERPRINT="scorpio-user 6.0.1 MXB48T V8.2.5.0.MADCNDL release-keys" \
-    PRIVATE_BUILD_DESC="Xiaomi/scorpio/scorpio:6.0.1/MXB48T/V8.2.5.0.MADCNDL:user/release-keys"
+    BUILD_FINGERPRINT="Xiaomi/scorpio/scorpio/6.0.1/MXB48T/V8.2.5.0.MADCNDL:user/release-keys" \
+    PRIVATE_BUILD_DESC="scorpio-user 6.0.1 MXB48T V8.2.5.0 MADCNDL release-keys"
